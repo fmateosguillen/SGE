8aa2e59408b218f9a0a95b403be1e72573a2469b
Francisco Javier Mateos Guillén <fmateosguillen@gmail.com>
2020-10-19 10:17:00 +0200

    Añadido Boletin de ejercicios

---
+++ pruebaMaven/src/test/java/org/example/AppTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertEquals(true, "A" == "A");
+    }
+
+}
